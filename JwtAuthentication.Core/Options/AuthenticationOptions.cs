93bcd575f28db28d035192d666309a05255ff4a4
Koval1997 <kovalmax06@gmail.com>
2021-01-13 15:04:48 +0500

    chore(access-token): add debug token handler and auth options

---
+++ JwtAuthentication.Core/Options/AuthenticationOptions.cs
@@ -0,0 +1,37 @@
+namespace JwtAuthentication.Core.Options
+{
+    public class AuthenticationOptions
+    {
+        private string _signingKey;
+        public string SigningKey
+        {
+            get => _signingKey ?? "jwtKeyjwtKeyjwtKeyjwtKeyjwtKey";
+            set => _signingKey = value;
+        }
+
+
+        private string _issuer;
+        public string Issuer
+        {
+            get => _issuer;
+            set => _issuer = value;
+        }
+
+        private int _accessTokenExpireInMinutes;
+        public int AccessTokenExpireInMinutes
+        {
+            get => _accessTokenExpireInMinutes == default ? 180 : _accessTokenExpireInMinutes;
+            set => _accessTokenExpireInMinutes = value;
+        }
+
+
+        private int _refreshTokenExpireInMinutes;
+        public int RefreshTokenExpireInMinutes
+        {
+            get => _refreshTokenExpireInMinutes == default ? 10080 : _refreshTokenExpireInMinutes;
+            set => _refreshTokenExpireInMinutes = value;
+        }
+
+        public bool IsDebugTokenEnabled { get; set; } 
+    }
+}
