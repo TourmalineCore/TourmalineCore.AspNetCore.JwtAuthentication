3e50d86280854a21def0fc984c7da6bdf59b6303
Koval1997 <kovalmax06@gmail.com>
2021-01-14 13:08:34 +0500

    chore(access-token): add Example project based on .Net Core 5.0 for base JWT usage

---
+++ Examples/Example.NetCore5.0/Startup.cs
@@ -0,0 +1,41 @@
+using JwtAuthentication.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Example.NetCore5._0
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddJwtAuthentication<long>();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseJwtAuthentication();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
