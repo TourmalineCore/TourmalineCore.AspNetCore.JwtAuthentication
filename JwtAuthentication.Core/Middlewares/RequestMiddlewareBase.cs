927080b61c36baec35a2c4f1aff1d215d4934fa3
Nikita Medvedev <nmedvedev@tourmalinecore.com>
2021-05-07 15:52:14 +0500

    feat(core). #23: multi-targeting added

--- JwtAuthentication.Core/Middlewares/RequestMiddlewareBase.cs
+++ JwtAuthentication.Core/Middlewares/RequestMiddlewareBase.cs
@@ -1,22 +1,31 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+#if NETCOREAPP3_0 || NETCOREAPP3_1
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+#elif NET5_0
+using System.Text.Json;
+using System.Text.Json.Serialization;
+#endif
 
 namespace TourmalineCore.AspNetCore.JwtAuthentication.Core.Middlewares
 {
     internal abstract class RequestMiddlewareBase<TService, TRequestModel, TResponseModel>
     {
         private readonly RequestDelegate _next;
-
+#if NETCOREAPP3_0 || NETCOREAPP3_1
         private readonly JsonSerializerSettings _jsonSerializerSettings;
+#elif NET5_0
+        private readonly JsonSerializerOptions _jsonSerializerSettings;
+#endif
 
         public RequestMiddlewareBase(RequestDelegate next)
         {
             _next = next;
 
+#if NETCOREAPP3_0 || NETCOREAPP3_1
             _jsonSerializerSettings = new JsonSerializerSettings
             {
                 Formatting = Formatting.Indented,
@@ -28,6 +37,20 @@ namespace TourmalineCore.AspNetCore.JwtAuthentication.Core.Middlewares
                 },
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
             };
+#elif NET5_0
+            _jsonSerializerSettings = new JsonSerializerOptions
+            {
+                IgnoreNullValues = true,
+                AllowTrailingCommas = true,
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters =
+                {
+                    new JsonStringEnumConverter(),
+                },
+            };
+#endif
         }
 
         public async Task InvokeAsyncBase(HttpContext context, TService service, string endpointRoute)
@@ -56,7 +79,11 @@ namespace TourmalineCore.AspNetCore.JwtAuthentication.Core.Middlewares
         protected async Task Response(HttpContext context, TResponseModel result)
         {
             context.Response.ContentType = "application/json; charset=UTF-8";
+#if NETCOREAPP3_0 || NETCOREAPP3_1
             await context.Response.WriteAsync(JsonConvert.SerializeObject(result, _jsonSerializerSettings));
+#elif NET5_0
+            await context.Response.WriteAsync(JsonSerializer.Serialize(result, _jsonSerializerSettings));
+#endif
             await context.Response.Body.FlushAsync();
         }
 
@@ -64,7 +91,11 @@ namespace TourmalineCore.AspNetCore.JwtAuthentication.Core.Middlewares
         {
             using (var reader = new StreamReader(request.Body))
             {
+#if NETCOREAPP3_0 || NETCOREAPP3_1
                 return JsonConvert.DeserializeObject<T>(await reader.ReadToEndAsync(), _jsonSerializerSettings);
+#elif NET5_0
+                return JsonSerializer.Deserialize<T>(await reader.ReadToEndAsync(), _jsonSerializerSettings);
+#endif
             }
         }
 
