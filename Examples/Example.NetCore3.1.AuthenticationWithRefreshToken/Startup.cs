f8ba385495e1d297409a5fdfc7a4eeef5ee3e09c
Vladislav Yusupov <vyusupov@tourmalinecore.com>
2022-08-17 11:47:52 +0500

    build(examples): #60: adds a project example for the refresh feature

---
+++ Examples/Example.NetCore3.1.AuthenticationWithRefreshToken/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Options;
+
+namespace Example.NetCore3._1.AuthenticationWithRefreshToken
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddJwtAuthentication(_configuration.GetSection(nameof(AuthenticationOptions)).Get<AuthenticationOptions>())
+                .AddLoginWithRefresh();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app
+                .UseDefaultLoginMiddleware()
+                .UseJwtAuthentication();
+
+            app.UseRefreshTokenMiddleware();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
