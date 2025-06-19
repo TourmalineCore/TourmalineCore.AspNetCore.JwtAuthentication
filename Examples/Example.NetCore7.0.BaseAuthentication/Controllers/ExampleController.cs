b6eba85293faa1407c6becbcef00415e7c864f7f
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-19 09:42:28 +0500

    chore(examples): #65: add example for net7 BaseAuthentication

---
+++ Examples/Example.NetCore7.0.BaseAuthentication/Controllers/ExampleController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.NetCore7._0.BaseAuthentication.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExampleController : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        return Ok("You are authenticated!");
+    }
+
+    [HttpGet("public")]
+    public IActionResult GetPublic()
+    {
+        return Ok("This endpoint is public");
+    }
+}
