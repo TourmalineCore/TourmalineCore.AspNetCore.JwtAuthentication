ea93fe24b79cc37b7e7a1083320a95355a2f8170
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-22 19:31:10 +0500

    chore(tests): #65: add tests for net7 projects

---
+++ Examples/Tests/NetCore7.0/RefreshTests.cs
@@ -0,0 +1,87 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models.Request;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models.Response;
+using TourmalineCore.AspNetCore.JwtAuthentication.Identity;
+using Xunit;
+
+namespace Tests.NetCore7._0
+{
+    public class RefreshTests
+        : AuthTestsBase<Program>
+    {
+        private const string LogoutUrl = "/auth/logout";
+
+        private const string Login = "Admin";
+        private const string Password = "Admin";
+        private const string FingerPrint = "fingerprint";
+
+        public RefreshTests(WebApplicationFactory<Program> factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task RefreshWithValidToken_ReturnsTokens()
+        {
+            var loginResult = await LoginAsync(Login, Password);
+
+            var (_, result) = await CallRefresh(loginResult.authModel);
+
+            Assert.False(string.IsNullOrWhiteSpace(result.AccessToken.Value));
+            Assert.False(string.IsNullOrWhiteSpace(result.RefreshToken.Value));
+        }
+
+        [Fact]
+        public async Task RefreshWithInvalidToken_Returns401()
+        {
+            var invalidAuthResponseModel = new AuthResponseModel
+            {
+                RefreshToken = new TokenModel
+                {
+                    Value = Guid.NewGuid().ToString(),
+                },
+                AccessToken = new TokenModel
+                {
+                    Value = string.Empty,
+                },
+            };
+
+            var (response, _) = await CallRefresh(invalidAuthResponseModel, Guid.NewGuid().ToString());
+            Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RefreshWithTheSameValidTokenMultipleTimes_Returns401()
+        {
+            var (_, authModel) = await LoginAsync(Login, Password, FingerPrint);
+
+            await CallRefresh(authModel, FingerPrint);
+            var (response, _) = await CallRefresh(authModel, FingerPrint);
+
+            Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task LogoutWithValidToken_DiscardsRefresh()
+        {
+            var loginResult = await LoginAsync(Login, Password);
+
+            var refreshResult = await CallRefresh(loginResult.authModel);
+
+            var client = _factory.CreateClient();
+
+            var body = JsonContent.Create(new RefreshTokenRequestModel
+                    {
+                        RefreshTokenValue = Guid.Parse(refreshResult.authModel.RefreshToken.Value),
+                    }
+                );
+
+            var logoutResult = await client.PostAsync(LogoutUrl, body);
+
+            Assert.Equal(HttpStatusCode.OK, logoutResult.StatusCode);
+        }
+    }
+}
---
+++ Examples/Tests/NetCore7.0/RegistrationTests.cs
@@ -0,0 +1,67 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models.Request;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Models.Response;
+using Xunit;
+
+namespace Tests.NetCore7._0
+{
+    public class RegistrationTests
+        : AuthTestsBase<Program>
+    {
+        private const string RegisterUrl = "/auth/register";
+
+        private const string Login = "test";
+        private const string Password = "Test1234.";
+
+        public RegistrationTests(WebApplicationFactory<Program> factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task RegisterWithInvalidCreds_Returns400()
+        {
+            var registerResult = await RegisterAsync(Login, "1");
+            Assert.Equal(HttpStatusCode.Conflict, registerResult.response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RegisterWithValidCreds_CreatesNewUser()
+        {
+            var registerResult = await RegisterAsync(Login, Password);
+
+            var callAuthorizedEndpointResult = await CallAuthorizedEndpointAsync(registerResult.authModel.AccessToken.Value);
+            Assert.Equal(HttpStatusCode.OK, callAuthorizedEndpointResult.StatusCode);
+
+            var loginResult = await LoginAsync(Login, Password);
+
+            Assert.False(string.IsNullOrWhiteSpace(loginResult.authModel.AccessToken.Value));
+        }
+
+        [Fact]
+        public async Task RegisterWithCredsOfExistingUser_Returns400()
+        {
+            var registerResult = await RegisterAsync("Admin", "Admin");
+            Assert.Equal(HttpStatusCode.Conflict, registerResult.response.StatusCode);
+        }
+
+        private async Task<(HttpResponseMessage response, AuthResponseModel authModel)> RegisterAsync(string login, string password)
+        {
+            var client = _factory.CreateClient();
+
+            var body = JsonContent.Create(new RegistrationRequestModel
+                    {
+                        Login = login,
+                        Password = password,
+                    }
+                );
+
+            var response = await client.PostAsync(RegisterUrl, body);
+            var result = JsonSerializer.Deserialize<AuthResponseModel>(response.Content.ReadAsStringAsync().Result, _jsonSerializerSettings);
+            return (response, result);
+        }
+    }
+}
